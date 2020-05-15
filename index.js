2bea5b0757db456224355abf35ce5da35b5cbf79
Flare-k <rokkyw@naver.com>
2020-05-15 19:32:18 +0900

    Add babel and middleware

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 const PORT = 80;
 
 function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
+
+const sayHello = (req, res) => res.send("Hello~!");
+
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get('/', sayHello);
 
 app.listen(PORT, handleListening);
