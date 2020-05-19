cc2f4806dccd1c7ea6bc18a004971ead505e44e9
Flare-k <rokkyw@naver.com>
2020-05-19 20:42:03 +0900

    [Modify] Login, Join, Search View

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,8 +5,8 @@ import { users, userDetail, editProfile, changePassword } from "../controllers/u
 const userRouter = express.Router();
 
 userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
