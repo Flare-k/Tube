5fe0152d79917594c796f0f2382ea8bc87def07d
Flare-k <rokkyw@naver.com>
2020-06-09 18:32:48 +0900

    [Test] SCSS and Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const autoprefixer = require("autoprefixer");
 const ExtractCSS = require("extract-text-webpack-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
@@ -6,30 +7,44 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: [ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
       {
-        test: /\.(scss|sass)$/,
-        use: ExtractCSS.extract(
+        test: /\.{js}$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(scss)$/,
+        use: ExtractCSS.extract([
           {
             loader: "css-loader",
           },
           {
             loader: "postcss-loader",
+            options: {
+              plugins() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
+              },
+            },
           },
           {
             loader: "sass-loader",
-          }
-        ),
+          },
+        ]),
       },
     ],
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]",
+    filename: "[name].js",
   },
+  plugins: [new ExtractCSS("style.css")],
 };
 
 module.exports = config;
