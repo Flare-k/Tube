8bb0eeeca52db3b7395d85502af8df371c1ef5e7
Flare-k <rokkyw@naver.com>
2020-06-09 18:42:31 +0900

    check babel

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ const app = express();
 app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); //json, html, text, urlencoded 할 거 없이 다 parser할 수 있도록 설정해줘야 한다.
+app.use(bodyParser.urlencoded({ extended: true })); // json, html, text, urlencoded 할 거 없이 다 parser할 수 있도록 설정해줘야 한다.
 app.use(morgan("dev"));
 
 app.use(localsMiddleware);
@@ -24,4 +25,4 @@ app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
-export default app; //파일을 불러올때 app object를 준다는 의미.
+export default app; // 파일을 불러올때 app object를 준다는 의미.
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: [ENTRY_FILE],
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
