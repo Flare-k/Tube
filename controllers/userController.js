1f3f6a50e84483d2710ca7c0d82d4f63a66c795e
Flare-k <rokkyw@naver.com>
2020-06-12 02:59:04 +0900

    [Modified] User controller Join

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
 import routes from "../routes";
+import User from "../models/User";
 
 // 회원가입 -> 완료 -> 홈화면으로 Redirect
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   const {
     body: { name, email, password, password2 },
   } = req;
@@ -12,6 +13,15 @@ export const postJoin = (req, res) => {
     res.status(400);
     res.render("join", { pageTitle: "Join" });
   } else {
+    try {
+      const user = await User.create({
+        name,
+        email,
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
+    }
     // To Do: Register User
     // To Do: Log user in
     res.redirect(routes.home);
